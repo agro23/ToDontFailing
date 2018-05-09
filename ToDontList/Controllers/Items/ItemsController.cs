9bddd7d026addc3a5914ba5ea40c171deb694d2c
Andy Grossberg <andy.grossberg@gmail.com>
2018-05-09 15:18:33 -0700

    Make File and Directory structure.

---
+++ ToDontList/Controllers/Items/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDontList.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ToDontList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDontListDbContext db = new ToDontListDbContext();
+       
+        public IActionResult Index()
+        {
+            List<Item> model = db.Items.ToList();
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+    }
+}
---
+++ ToDontList/Models/ToDontListDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDontList.Models
+{
+    public class ToDontListDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=localhost;Port=8889;database=todontlist;uid=root;pwd=root;");
+    }
+}
